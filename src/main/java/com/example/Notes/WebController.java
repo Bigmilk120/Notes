7032b75a70a480d20ca9ef46633b7a750b108234
Bigmilk120 <sprawdzany96@gmail.com>
2019-03-02 18:28:43 +0100

    .

--- src/main/java/com/example/Notes/WebController.java
+++ src/main/java/com/example/Notes/WebController.java
@@ -3,6 +3,8 @@ package com.example.Notes;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -15,8 +17,13 @@ public class WebController {
     }
 
     @GetMapping("/")
-    public String index(@RequestParam(name="name", required=false, defaultValue="Krzysiek") String name, Model model){
-        model.addAttribute("name", name);
-        return "index";
+    public String Login(Model model) {
+        model.addAttribute("Login", new Login());
+        return "Login";
+    }
+
+    @PostMapping("/")
+    public String Login(@ModelAttribute Login login) {
+        return "result";
     }
 }
