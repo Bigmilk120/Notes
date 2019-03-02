ec8a6da83ca99559cf00a6853449684db644e34b
Bigmilk120 <sprawdzany96@gmail.com>
2019-03-02 13:05:47 +0100

    Made web page connection.

---
+++ src/main/java/com/example/Notes/WebController.java
@@ -0,0 +1,16 @@
+package com.example.Notes;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/webController")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="Just") String name, Model model) {
+        model.addAttribute("name", name);
+        return "webController";
+    }
+}
