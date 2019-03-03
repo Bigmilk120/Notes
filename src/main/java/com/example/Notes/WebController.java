75ed0e846ebd286032a0d049cc7613b9971f4281
Bigmilk120 <sprawdzany96@gmail.com>
2019-03-03 18:45:07 +0100

    Made the form with sending variables.

--- src/main/java/com/example/Notes/WebController.java
+++ src/main/java/com/example/Notes/WebController.java
@@ -2,7 +2,11 @@ package com.example.Notes;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 public class WebController {
@@ -26,7 +30,10 @@ public class WebController {
 
 
     @RequestMapping("/Result")
-    public String Result(@RequestParam(name="username", required=false, defaultValue="Just") String username,@RequestParam(name="password", required=false, defaultValue="Just") String password){
-        return "result";
+    public String Result(@Valid @ModelAttribute("employee")Login login, BindingResult result, ModelMap model){
+        model.addAttribute("username",login.getUsername());
+        model.addAttribute("password",login.getPassword());
+
+        return "Result";
     }
 }
